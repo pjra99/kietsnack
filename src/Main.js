2dfbf555a653bed19d110bac2497d77c3bd4f72e
pranjalrai-art <62153142+pranjalrai-art@users.noreply.github.com>
2021-04-15 17:02:54 +0530

    Update Main.js

--- src/Main.js
+++ src/Main.js
@@ -17,16 +17,7 @@ import './App.css'
 class Main extends React.Component {
 
      render(){
-        const buttonStyle ={
-                padding: '10px',
-                marginLeft: '20px',
-                borderRadius: '50px',
-                border: 'none',
-                fontFamily: 'Pragati Narrow',
-                boxShadow: '8px 10px 10px #f89d13',
-                fontFamily: 'Lato'
-        }
-            
+ 
             const liStyle = {
                 display: 'inline',
                 marginRight: '30px'
@@ -35,18 +26,35 @@ class Main extends React.Component {
                 ReactDOM.render(<SignIn />,document.getElementById('root'));
              }
          return (
-            <div> 
-                <div className="header"  style={{
+            <div class="container-fluid"> 
+                <div className="row row1"  style={{
                     backgroundColor: 'rgb(255,196,60)',
-                    padding: '2%',
+                    // padding: '2%',
                     fontFamily:'Fredoka One',}}>
-                <h1> KIET Foods! <FaHamburger /> <FaUtensils /></h1>
-                <h3 style={{paddingBottom: '20px'}}>
+                <div className="col-lg-4" style={{
+                    fontSize: '40px',
+                    marginLeft: '2%',
+                    // paddingBottom: '-2%'
+                }}>KIET Foods! <FaHamburger /> <FaUtensils />
+                </div> 
+                <div className="col-lg-8">
+
+                </div>
+                </div>
+                <div className="row" style={{
+                    backgroundColor: 'rgb(255,196,60)',
+                    padding: '0 2% 2%   2%',
+                    fontFamily:'Fredoka One',}}>
+                {/* <div className="row"> */}
+                <div className="col-lg-6" style={{
+                    // paddingBottom: '20px',
+                             fontSize: '20px'}}>
                      An online site for ordering food from Kiet's canteens
-                     </h3>
+                     </div>
+                     <div className="col-lg-6">
                      <ul style={{
                          float:'right',
-                         marginTop: '-40px'
+                        //  marginTop: '-40px'
                      }}>
                     <li  style={liStyle} className='list' onMouseOver="li.Style.style={{
                         color: 'red'
@@ -55,61 +63,106 @@ class Main extends React.Component {
                     <li  style={liStyle} className='list'>Contact Developer</li>
                 </ul>
                 </div>
-                <div style={{backgroundColor: 'rgb(255,196,60)', paddingBottom: '10px'}}>
-                <Frames />
-                <h1 style={{
+                {/* </div> */}
+                </div>
+
+                <div className="row row2" style={{backgroundColor: 'rgb(255,196,60)', paddingBottom: '10px'}}>
+                <div className="col-lg-5"><Frames /></div>
+               <div className="col-lg-7"> 
+               <div  className="row" style={{
                     float: 'right',
-                    marginTop:'-320px',
+                    // marginTop:'-320px',
+                    fontSize: '30px',
                     marginRight: '70px',
                     fontFamily: 'Vollkorn',
-                    color: '#252525'
+                    color: '#252525',
                 }}>Get your favourite canteen food <br />delivered at your hostel! <br />
-                    </h1>
-                    <div class="buttons" style={{
-                    float: 'right',
-                    marginRight: '190px',
-                    marginTop: '-180px'
-                    }}>        
-                    <button className='signIn' type="submit" style={buttonStyle} onClick = {handleSignIn}>
-                            Sign in as Student<BsFillPersonFill />
-                        </button>
-                      <button className='signIn' type="submit" style={buttonStyle}>
-                            Sign in as Administrator <RiAdminLine />
-                        </button>
-                    <br />
-                        </div>
-                        <input type="text" placeholder="What would you like to eat?" class='search'
+                    </div>
+                    {/* <div className="row"> */}
+                      <div className="row"> 
+                      <div className="col-md-7" style={{
+                          float: 'right'
+                      }}>
+                      <input type="text" placeholder="What would you like to eat?" className='search'
                         style={{
-                        float: 'right',
-                        marginRight: '240px',
-                        marginTop: '-110px',
+                        // float: 'right',
+                        // marginRight: '240px',
+                        // marginTop: '-110px',
                         border: 'none',
                         backgroundColor: '#efecec',
                         width: '220px',
                         height: '35px',
                         borderRadius : '40px'
                         }}/>
-                             <button  style={{
-                            float: 'right',
-                            marginRight: '250px',
-                            marginTop: '-100px',   
-                            border: 'none'
+                        <button  style={{   
+                            border: 'none',
+                            marginLeft: '-30px'
                         }}
                         onClick={()=> ReactDOM.render(<SignIn />, document.getElementById('root'))} >
-                            <BsSearch/>
-                        </button> 
+                            <BsSearch />
+                        </button>
+                      </div>
+                        </div> 
+                        {/* <div className="row"> 
+                      
+                        </div>   */}
+                        <div className="row buttons" style={{
+                            // display: 'flex',
+                            // justifyContent: 'right',
+                    //  margin: 'auto',
+                    // textAlign: 'center',
+                    float: 'right',
+                    paddingTop: '5%',
+                    }}>        
+            <div className="col-sm-3" style={{
+            }}>
+                <button className='signIn' type="submit" style={{
+                     padding: '10px',   
+                     marginRight: '-50px',
+                     width: '200px',
+                     borderRadius: '50px',
+                     border: 'none',
+                     fontFamily: 'Pragati Narrow',
+                     boxShadow: '8px 10px 10px #f89d13',
+                     fontFamily: 'Lato',
+                     }} onClick = {handleSignIn}>
+                            Sign in as Student<BsFillPersonFill />
+                        </button>
+                        </div>        
+                     <div className="col-sm-4">
+                         <button className='signIn' type="submit" style={{
+                              padding: '10px',
+                              marginLeft:'100px',
+                              width: '200px',
+                              borderRadius: '50px',
+                              border: 'none',
+                              fontFamily: 'Pragati Narrow',
+                              boxShadow: '8px 10px 10px #f89d13',
+                              fontFamily: 'Lato'
+                         }}>
+                            Sign in as Administrator <RiAdminLine />
+                        </button>
+                        </div> 
+                    {/* <br /> */}
+                        </div>
                 </div>
-                <div className="bottom-upper">
+                </div>
+                <div className="row bottom-upper">
                     <h2 style={{
-                        marginLeft: '22%',
-                        marginTop: '20px',
+                        // marginLeft: '22%',
+                        textAlign: 'center',
+                        // marginTop: '20px',   
                         fontFamily: 'Vollkorn',
                     }}> Order from any of your favourite spots around your college</h2>
-                    <Canteens img={bigtreat} height='250px' width='350px'/>
-                    <Canteens img={cafeteria} /> <br/>
-                    <Canteens img={nishu} />
-                    <Canteens img={heroes} />
-                </div>
+                    </div>
+                    <div className="row">
+                   <div className="col-md-6"><Canteens img={bigtreat} /></div> 
+                   <div className="col-md-6"><Canteens img={cafeteria} /> </div> 
+                    </div>
+                    <div className="row" >
+                        <div className="col-md-6"> <Canteens img={heroes} /></div>
+                        <div className="col-md-6"> <Canteens img={nishu} /> </div>
+                    </div>
                 <div className="bottom-lower" style={{
                     height: '100px',
                     backgroundColor: '#eeeeee'
