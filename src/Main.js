07f3d087e5ac5d58e93a87fdb997c249bb9a9dbe
Pranjal Rai <62153142+pjra99@users.noreply.github.com>
2021-04-30 23:30:19 +0530

    Update Main.js

--- src/Main.js
+++ src/Main.js
@@ -12,165 +12,86 @@ import bigtreat from "./images/bigtreat.jpg"
 import cafeteria from "./images/cafeteria.jpg"
 import nishu from "./images/nishu.jpg"
 import heroes from "./images/heroes.jpg"
-import './App.css'
+import './Main.css'
  
 class Main extends React.Component {
 
      render(){
- 
-            const liStyle = {
-                display: 'inline',
-                marginRight: '30px'
-            }
+
             function handleSignIn() {
                 ReactDOM.render(<SignIn />,document.getElementById('root'));
              }
          return (
             <div class="container-fluid"> 
-                <div className="row row1"  style={{
-                    backgroundColor: 'rgb(255,196,60)',
-                    // padding: '2%',
-                    fontFamily:'Fredoka One',}}>
-                <div className="col-lg-4" style={{
-                    fontSize: '40px',
-                    marginLeft: '2%',
-                    // paddingBottom: '-2%'
-                }}>KIET Foods! <FaHamburger /> <FaUtensils />
+                <div className="row row1">
+                <div className="col-lg-4 heading-title">KIET Foods! <FaHamburger /> <FaUtensils />
                 </div> 
                 <div className="col-lg-8">
 
                 </div>
                 </div>
-                <div className="row" style={{
-                    backgroundColor: 'rgb(255,196,60)',
-                    padding: '0 2% 2%   2%',
-                    fontFamily:'Fredoka One',}}>
-                {/* <div className="row"> */}
-                <div className="col-lg-6" style={{
-                    // paddingBottom: '20px',
-                             fontSize: '20px'}}>
+                <div className="row heading-lower">
+                <div className="col-lg-6 heading-lower-phrase">
                      An online site for ordering food from Kiet's canteens
                      </div>
                      <div className="col-lg-6">
-                     <ul style={{
-                         float:'right',
-                        //  marginTop: '-40px'
-                     }}>
-                    <li  style={liStyle} className='list' onMouseOver="li.Style.style={{
-                        color: 'red'
-                    }}">Home</li>
-                    <li  style={liStyle} className='list'>All Canteens</li>
-                    <li  style={liStyle} className='list'>Contact Developer</li>
+                     <ul className="header-list">
+                    <li className='list'>Home</li>
+                    <li className='list'>All Canteens</li>
+                    <li className='list'>Contact Developer</li>
                 </ul>
                 </div>
-                {/* </div> */}
                 </div>
-
-                <div className="row row2" style={{backgroundColor: 'rgb(255,196,60)', paddingBottom: '10px'}}>
+                <div className="row row2">
                 <div className="col-lg-5"><Frames /></div>
                <div className="col-lg-7"> 
-               <div  className="row" style={{
-                    float: 'right',
-                    // marginTop:'-320px',
-                    fontSize: '30px',
-                    marginRight: '70px',
-                    fontFamily: 'Vollkorn',
-                    color: '#252525',
-                }}>Get your favourite canteen food <br />delivered at your hostel! <br />
+               <div  className="row header-center-title"> 
+               Get your favourite canteen food <br />delivered at your hostel! <br />
                     </div>
-                    {/* <div className="row"> */}
                       <div className="row"> 
-                      <div className="col-md-7" style={{
-                          float: 'right'
-                      }}>
-                      <input type="text" placeholder="What would you like to eat?" className='search'
-                        style={{
-                        // float: 'right',
-                        // marginRight: '240px',
-                        // marginTop: '-110px',
-                        border: 'none',
-                        backgroundColor: '#efecec',
-                        width: '220px',
-                        height: '35px',
-                        borderRadius : '40px'
-                        }}/>
-                        <button  style={{   
-                            border: 'none',
-                            marginLeft: '-30px'
-                        }}
+                      <div className="col-md-5"></div>
+                      <div className="col-md-5">
+                      <input type="text" placeholder="What would you like to eat?" className='search' />
+                      <button className="search-button"
                         onClick={()=> ReactDOM.render(<SignIn />, document.getElementById('root'))} >
                             <BsSearch />
                         </button>
-                      </div>
-                        </div> 
-                        {/* <div className="row"> 
                       
-                        </div>   */}
-                        <div className="row buttons" style={{
-                            // display: 'flex',
-                            // justifyContent: 'right',
-                    //  margin: 'auto',
-                    // textAlign: 'center',
-                    float: 'right',
-                    paddingTop: '5%',
-                    }}>        
-            <div className="col-sm-3" style={{
-            }}>
-                <button className='signIn' type="submit" style={{
-                     padding: '10px',   
-                     marginRight: '-50px',
-                     width: '200px',
-                     borderRadius: '50px',
-                     border: 'none',
-                     fontFamily: 'Pragati Narrow',
-                     boxShadow: '8px 10px 10px #f89d13',
-                     fontFamily: 'Lato',
-                     }} onClick = {handleSignIn}>
+                        </div>
+                      <div className="col-md-4"></div>
+                        </div> 
+                        <div className="row buttons">    
+                        <div className="col-sm-3"></div>    
+            <div className="col-sm-3">
+                <button className='signInStu' type="submit" onClick = {handleSignIn}>
                             Sign in as Student<BsFillPersonFill />
                         </button>
                         </div>        
-                     <div className="col-sm-4">
-                         <button className='signIn' type="submit" style={{
-                              padding: '10px',
-                              marginLeft:'100px',
-                              width: '200px',
-                              borderRadius: '50px',
-                              border: 'none',
-                              fontFamily: 'Pragati Narrow',
-                              boxShadow: '8px 10px 10px #f89d13',
-                              fontFamily: 'Lato'
-                         }}>
+                        <div className="col-sm-1"></div>
+                     <div className="col-sm-3">
+                         <button className='signInAdm' type="submit">
                             Sign in as Administrator <RiAdminLine />
                         </button>
                         </div> 
-                    {/* <br /> */}
+                        <div className="col-sm-2"></div>    
+
                         </div>
                 </div>
                 </div>
                 <div className="row bottom-upper">
-                    <h2 style={{
-                        // marginLeft: '22%',
-                        textAlign: 'center',
-                        // marginTop: '20px',   
-                        fontFamily: 'Vollkorn',
-                    }}> Order from any of your favourite spots around your college</h2>
+                    <h2 className="bottom-upper-phrase" > Order from any of your favourite spots around your college</h2>
                     </div>
                     <div className="row">
-                   <div className="col-md-6"><Canteens img={bigtreat} /></div> 
-                   <div className="col-md-6"><Canteens img={cafeteria} /> </div> 
+                   <div className="col-md-6 img1"><Canteens img={bigtreat} /></div> 
+                   <div className="col-md-6 img2"><Canteens img={cafeteria} /> </div> 
                     </div>
                     <div className="row" >
-                        <div className="col-md-6"> <Canteens img={heroes} /></div>
-                        <div className="col-md-6"> <Canteens img={nishu} /> </div>
+                        <div className="col-md-6 img3"> <Canteens img={heroes} /></div>
+                        <div className="col-md-6 img4"> <Canteens img={nishu} /> </div>
                     </div>
-                <div className="bottom-lower" style={{
-                    height: '100px',
-                    backgroundColor: '#eeeeee'
-                }} align='center'>
-                    <h4 
-                    style={{paddingTop:'40px',
-                opacity: '.8'}}
-                    >KIET FOODS | All Rights Reserved &copy;</h4>
+                <div className="bottom-lower">
+                    <h4  className="bottom-lower-phrase">
+               KIET FOODS | All Rights Reserved &copy;</h4>
                 </div>
             </div>
          )
